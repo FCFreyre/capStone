bce82390b0be641b9c19d678797e66ed6e2e3d42
F. Coulter Freyre <fcoulterfreyre@gmail.com>
2018-03-27 19:05:43 -0600

    added cors.anywhere

--- src/actions.js
+++ src/actions.js
@@ -3,7 +3,8 @@ export const ADD_PLAY = 'ADD_PLAY';
 export const PLAY_FETCHED = 'PLAY_FETCHED';
 export const PLAY_UPDATED = 'PLAY_UPDATED';
 export const PLAY_DELETED = 'PLAY_DELETED';
-const API_BASE_URL = /localhost/.test(window.location.href) ? 'http://localhost:8080' : 'https://bref-mandarine-14323.herokuapp.com'
+const API_BASE_URL = /localhost/.test(window.location.href) ? 'http://localhost:8080' : 'https://cors-anywhere.herokuapp.com/https://bref-mandarine-14323.herokuapp.com'
+
 
 function handleResponse(response) {
   if (response.ok) {
@@ -94,7 +95,7 @@ export function deletePlay(id) {
 
 export function fetchPlays() {
   return dispatch => {
-    fetch(API_BASE_URL + '/api/plays')
+    fetch(API_BASE_URL + '/api/plays', {'mode': 'no-cors'})
     .then(res => res.json())
     .then(data =>
       dispatch(setPlays(data.plays)));
